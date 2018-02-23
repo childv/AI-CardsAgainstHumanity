18066ce5f45cdea43960db59d90d1a97c2e23dd4
SamWiseman <wisemans@carleton.edu>
2018-02-23 01:31:27 -0600

    works with single cards

---
+++ cards.py
@@ -0,0 +1,50 @@
+#cards.py
+from random import *
+from copy import *
+import sys
+
+def main():
+	questions = open('against-humanity/questions.txt').readlines()
+	answers = open('against-humanity/answers.txt').readlines()
+	blackDeck = [question[:-1] for question in questions]
+	whiteDeck = [answer[:-1] for answer in answers]
+	going = True
+	while going == True:
+		chosenBlack = blackDeck[randrange(0, len(blackDeck))]
+		numOptions = 5 if len(sys.argv) == 1 else sys.argv[1]
+		currentDeck = deepcopy(whiteDeck)
+		options = []
+		print("Black card:", chosenBlack)
+		print("Which option best completes the blank?")
+		for i in range(numOptions):
+			numCards = len(whiteDeck)
+			if numCards == 0:
+				print("The deck is empty.")
+				break
+			else:
+				index = randrange(0, len(whiteDeck))
+				options.append(currentDeck.pop(index))
+		for i in range(len(options)):
+			print("Option " + str(i+1) + ": " + options[i])
+		invalid = True
+		while invalid == True:
+			choice = input("Which number card would you like to play?\n")
+			if int(choice) - 1 in range(numOptions):
+				invalid = False
+			else:
+				print("Invalid choice.") 
+		blank = chosenBlack.find('_')
+		goodPhrase = options[int(choice) - 1].upper()
+		insertion = goodPhrase[:-1] if goodPhrase[-1] == "." and blank >= 0 \
+		 else goodPhrase
+		if blank == -1:
+			result = chosenBlack + ' ' + insertion
+		result = chosenBlack[:blank] + insertion + chosenBlack[blank + 1:]
+		print(result)
+		persist = input("Play again? (y/n)\n")
+		going = True if persist == "y" else False
+
+
+
+if __name__ == '__main__':
+	main()
