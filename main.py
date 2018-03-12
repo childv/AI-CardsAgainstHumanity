906b7e12516521168baa1658ae76ad41753896c2
SamWiseman <wisemans@carleton.edu>
2018-03-12 16:55:15 -0500

    it choose\!

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def makePlayers(game):
         player.makeHand(numOptions, whiteDeck)
     return players
 
-def chooseBest(playerWithBlack, choices):
+def humanChooseBest(playerWithBlack, choices):
     shuffle(choices)
     print("The results are in! In no particular order: ")
     for i in range(len(choices)):
@@ -45,8 +45,20 @@ def chooseBest(playerWithBlack, choices):
     choice = int(choice)
     if choice - 1 not in range(len(choices)):
         print("Please input an integer between 1 and " + str(len(choices)) + ".")
-        return self.getChoice(hand, priorChoices, choice)
+        return chooseBest(playerWithBlack, choices)
     return choices[choice-1][0]
+
+def aiChooseBest(playerWithBlack, choices):
+    shuffle(choices)
+    humor_prediction = predict_batch(choices)
+    print("The results are in! In no particular order: ")
+    for i in range(len(choices)):
+        print ("Option " + str(i+1) + ": " + choices[i][1] 
+            + " -> " + str(humor_prediction[i]))
+    print("")
+    funniest_sentence = np.argmax(humor_prediction)
+    print("The AI chose option " + str(funniest_sentence + 1) + ".")
+    return choices[funniest_sentence][0]
 
 def showScores(players):
     for player in players:
@@ -63,7 +75,8 @@ def runRound(game, players, playerTurn):
     game.get_black_card()
     print("\n\n\n\n\n---------- NEW ROUND ----------")
     blackPlayerNum = str(playerWithBlack.getNum())
-    input("Player " + blackPlayerNum + ", press return to draw a black card.")
+    if not blackPlayerNum.isAI():
+        input("Player " + blackPlayerNum + ", press return to draw a black card.")
     print("Player " + blackPlayerNum + " drew this card:")
     print(game.get_chosen_black())
     choices = []
@@ -76,8 +89,10 @@ def runRound(game, players, playerTurn):
         choice = ai_game(game, player) if AI else game.runTurn(player)
         choices.append((player.getNum(), choice))
         print("\n\n\n\n")
-    playerWhoWins = chooseBest(playerWithBlack, choices)
-    print("The card " + game.get_chosen_black() + " goes to player " + str(playerWhoWins) + "!")
+    blackAI = playerWithBlack.isAI()
+    playerWhoWins = aiChooseBest(playerWithBlack, choices) if blackAI \
+     else humanChooseBest(playerWithBlack, choices)
+    print('The card "' + game.get_chosen_black() + '" goes to player ' + str(playerWhoWins) + "!")
     players[playerWhoWins-1].raiseScore(game.get_chosen_black())
 
 def main():
@@ -91,7 +106,7 @@ def main():
         playerTurn = (playerTurn+1) % len(players)
         if playerTurn == 0:
             print("Now would be a fair time to stop.")
-        persist = input("Play again? (y/n)\n")
+        persist = input("Continue? (y/n)\n")
         going = True if persist == "y" else False
     showScores(players)
     #ai_game(game)
